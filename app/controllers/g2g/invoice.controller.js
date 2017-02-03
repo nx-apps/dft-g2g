b042242eaa2ba4b6afd6318879516d9c49aa962a
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-02-03 14:35:46 +0700

    update insert invoice

--- app/controllers/g2g/invoice.controller.js
+++ app/controllers/g2g/invoice.controller.js
@@ -290,6 +290,12 @@ exports.insert = function (req, res) {
     if (valid) {
         r.db("g2g").table("invoice")
             .insert(req.body)
+            .do(inv_do => {
+                return r.db('g2g').table('invoice').get(inv_do('generated_keys')(0))
+                    .do(book_do => {
+                        return r.db('g2g').table('invoice').get(book_do('book_id')).update({ book_status: true })
+                    })
+            })
             .run()
             .then(function (response) {
                 result.message = response;
