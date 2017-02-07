4aac34fcbaa10636089a2338bf5fe0e975cec5d7
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-02-07 09:55:01 +0700

    fix bug

--- app/controllers/g2g/contract.controller.js
+++ app/controllers/g2g/contract.controller.js
@@ -146,7 +146,7 @@ exports.insert = function (req, res) {
     var r = req._r;
     var result = { result: false, message: null, id: null };
     if (valid) {
-        var obj = Object.assign(req.body, { date_created: new Date().toISOString(), date_updated: new Date().toISOString(), creater: 'admin' :updater: 'admin' });
+        var obj = Object.assign(req.body, { date_created: new Date().toISOString(), date_updated: new Date().toISOString(), creater: 'admin', updater: 'admin' });
         r.db("g2g").table("contract")
             .insert(obj)
             .run()
