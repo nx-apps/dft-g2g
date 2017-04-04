c31659734db672c7db1c158c68f68c5b5dffe857
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-04-04 13:40:31 +0700

    confirm/exporter

--- app/controllers/g2g/confirm.controller.js
+++ app/controllers/g2g/confirm.controller.js
@@ -70,6 +70,68 @@ exports.getById = function (req, res) {
             res.json(err)
         })
 }
+exports.getByExporterId = function (req, res) {
+    var r = req.r;
+    r.db('g2g2').table('confirm_letter')
+        .getAll(req.query.contract_id, { index: 'contract_id' })
+        .filter({ cl_status: true })
+        .map(function (cl) {
+            return {
+                cl_id: cl('id'),
+                cl_no: cl('cl_no'),
+                cl_type_rice: cl('cl_type_rice')
+                    .merge(function (mmm) {
+                        return r.db('common').table('type_rice').get(mmm('type_rice_id')).without('id', 'creater', 'updater', 'date_created', 'date_updated')
+                    })
+                    .merge(function (limit) {
+                        return {
+                            type_rice_quantity_confirm: r.db('g2g2').table('shipment_detail')
+                                .getAll(cl('id'), { index: 'tags' })
+                                //.eqJoin("shm_id", r.db('g2g2').table("shipment")).without({ right: ["id", "date_created", "date_updated", "creater", "updater", "tags"] }).zip()
+                                .filter({
+                                    //cl_id: m('cl_id'),
+                                    type_rice_id: limit('type_rice_id')
+                                })
+                                .coerceTo('array')
+                                .getField('shm_det_quantity')
+                                .reduce(function (left, right) {
+                                    return left.add(right);
+                                }).default(0)
+                        }
+                    })
+                    .merge(function (mmm) {
+                        return {
+                            package: mmm('package').map(function (arr_package) {
+                                return arr_package.merge(function (row_package) {
+                                    return r.db('common').table('package').get(row_package('package_id')).without('id', 'creater', 'updater', 'date_created', 'date_updated')
+                                })
+                            }),
+                            type_rice_quantity_min: mmm('type_rice_quantity').sub(mmm('type_rice_quantity').mul(mmm('tolerance_rate').div(100)
+                            )),
+                            type_rice_quantity_max: mmm('type_rice_quantity').mul(mmm('tolerance_rate').div(100)
+                            ).add(mmm('type_rice_quantity')),
+                            type_rice_quantity_limit_min: mmm('type_rice_quantity').sub(mmm('type_rice_quantity').mul(mmm('tolerance_rate').div(100)
+                            )).sub(mmm('type_rice_quantity_confirm')),
+                            type_rice_quantity_limit_max: mmm('type_rice_quantity').mul(mmm('tolerance_rate').div(100)
+                            ).add(mmm('type_rice_quantity')).sub(mmm('type_rice_quantity_confirm'))
+                        }
+                    }),
+                cl_quantity_total: cl('cl_type_rice').sum('type_rice_quantity'),
+                cl_status: cl('cl_status'),
+                book_quantity: r.table('shipment_detail')
+                    .getAll(req.query.exporter_id, { index: 'exporter_id' })
+                    .filter(function (f) {
+                        return f('tags').contains(cl('id'))
+                    })
+                    .sum('shm_det_quantity')
+            }
+        })
+        .orderBy('cl_no')
+        .run()
+        .then(function (result) {
+            res.json(result);
+        })
+}
 exports.insert = function (req, res) {
     var valid = req.ajv.validate('g2g.confirm_letter', req.body);
     var r = req.r;
--- app/routes/g2g/confirm.routes.js
+++ app/routes/g2g/confirm.routes.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     var controller = require('../../controllers/g2g/confirm.controller');
     app.get('/contract/id/:contract_id', controller.getByContractId);
     app.get('/id/:cl_id', controller.getById);
-
+    app.get('/exporter', controller.getByExporterId)
     app.post('/insert', controller.insert);
     app.put('/update', controller.update);
     app.delete('/delete/id/:id', controller.delete);
