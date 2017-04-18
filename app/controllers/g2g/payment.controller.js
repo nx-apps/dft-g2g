6baafbb47566a4d79218547f6d9820ab5dec93bc
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-04-18 13:19:43 +0700

    use `statemnet`

--- app/controllers/g2g/payment.controller.js
+++ app/controllers/g2g/payment.controller.js
@@ -130,16 +130,16 @@ exports.getByFeeId = function (req, res) {
 }
 exports.getSilo = function (req, res) {
     req.jdbc.query("mssql",
-        "SELECT bk.Keyword as keyword,bk.Status as statusName,sum(p.tWeight) as weightAll "
-        + " FROM ( "
-        + " SELECT s.Keyword ,s.Status ,b.book_id "
-        + " FROM  dft_lk_status s "
-        + " INNER JOIN dft_booking b on b.status_id = s.Id "
-        + " WHERE s.Keyword like ? AND s.Active like ? "
-        + " GROUP BY  s.Keyword,s.Status,b.book_id "
-        + " ) bk "
-        + " INNER JOIN dft_product p on p.status = bk.book_id "
-        + " GROUP BY  bk.Keyword,bk.Status "
+   `  SELECT bk.Keyword as keyword,bk.Status as statusName,sum(p.tWeight) as weightAll 
+         FROM ( 
+         SELECT s.Keyword ,s.Status ,b.book_id 
+         FROM  dft_lk_status s 
+         INNER JOIN dft_booking b on b.status_id = s.Id 
+         WHERE s.Keyword like ? AND s.Active like ? 
+         GROUP BY  s.Keyword,s.Status,b.book_id 
+         ) bk 
+         INNER JOIN dft_product p on p.status = bk.book_id 
+         GROUP BY  bk.Keyword,bk.Status `
         ,
         ["GG%", "W%"],
         function (err, data) {
