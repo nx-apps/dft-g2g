deaeed01be2b4921f7993a8e125932127965b344
tonsai-j <tonsai.j@nexts-corp.com>
2017-04-18 12:02:37 +0700

    update label bl

--- config/config.js
+++ config/config.js
@@ -42,7 +42,7 @@ var config = {
       {
         name: "mssql",
         driver: "com.microsoft.sqlserver.jdbc.SQLServerDriver",
-        url: "jdbc:sqlserver://192.168.10.112:1433;databaseName=RiceDB",
+        url: "jdbc:sqlserver://192.168.10.115:1433;databaseName=RiceDB",
         user: "riceuser",
         password: "l2ice2015"
       },
