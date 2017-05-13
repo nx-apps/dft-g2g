919139f2469340ce6139eb44526c48517228fe60
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-05-13 13:38:33 +0700

    add noti

--- app/routes/g2g/payment.routes.js
+++ app/routes/g2g/payment.routes.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var controller = require('../../controllers/g2g/payment.controller');
+    app.get('/noti/contract/id/:contract_id',controller.getNotiByContractId);
     app.get('/contract/id/:contract_id', controller.getByContractId);
     app.get('/fee/id/:fee_id', controller.getByFeeId);
     app.get('/silo', controller.getSilo);
