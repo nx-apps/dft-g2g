65c72cda40b39aaf53d0083c294b2af513505668
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-06-12 21:46:38 +0700

    new g2g

--- app/routes/contract.routes.js
+++ app/routes/contract.routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     var controller = require('../controllers/contract.controller');
-    app.get('/list', controller.list2);
+    app.get('/list', controller.list2);  //updated 2017-06-12
     app.get('/buyer/id/:buyer_id', controller.buyerId);
     app.get('/id/:contract_id', controller.getById);
     app.post('/insert', controller.insert);
