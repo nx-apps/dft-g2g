89529d43abb2d709650c4c5909f8ba5125c78248
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-06-27 16:41:52 +0700

    update 2

--- public/script/reduxStore/commonG2g.store.js
+++ public/script/reduxStore/commonG2g.store.js
@@ -1,12 +1,43 @@
 import axios from '../axios'
 import { commonAction } from '../config'
 const initialState = {
-    list: []
+    bankList: [],
+    buyerList: [],
+    hamonizeList: [],
+    carrierList: [],
+    incotermsList: [],
+    notifyList: [],
+    packageList: [],
+    portList: [],
+    shipList: [],
+    shiplineList: [],
+    surveyorList: [],
+
 }
 export function commonG2gReducer(state = initialState, action) {
     switch (action.type) {
-        // case 'EXPORTER_GET_DATA':
-        //     return Object.assign({}, state, { list: action.payload });
+        case 'GET_COMMON_BANK_LIST':
+            return Object.assign({}, state, { bankList: action.payload });
+        case 'GET_COMMON_BUYER_LIST':
+            return Object.assign({}, state, { buyerList: action.payload });
+        case 'GET_COMMON_HAMONIZE_LIST':
+            return Object.assign({}, state, { hamonizeList: action.payload });
+        case 'GET_COMMON_CARRIER_LIST':
+            return Object.assign({}, state, { carrierList: action.payload });
+        case 'GET_COMMON_INCOTERMS_LIST':
+            return Object.assign({}, state, { incotermsList: action.payload });
+        case 'GET_COMMON_NOTIFY_LIST':
+            return Object.assign({}, state, { notifyList: action.payload });
+        case 'GET_COMMON_PACKAGE_LIST':
+            return Object.assign({}, state, { packageList: action.payload });
+        case 'GET_COMMON_PORT_LIST':
+            return Object.assign({}, state, { portList: action.payload });
+        case 'GET_COMMON_SHIP_LIST':
+            return Object.assign({}, state, { shipList: action.payload });
+        case 'GET_COMMON_SHIPLINE_LIST':
+            return Object.assign({}, state, { shiplineList: action.payload });
+        case 'GET_COMMON_SURVEYOR_LIST':
+            return Object.assign({}, state, { surveyorList: action.payload });
         default:
             return state
     }
@@ -14,8 +45,121 @@ export function commonG2gReducer(state = initialState, action) {
 export function commonG2gAction(store) {
     return [commonAction(),
     {
-        UPLOAD_GET_LIST: function (ref, com) {
-            // return axios.get('/external/upload/list/' + ref + '/' + com)
+
+        GET_COMMON_BANK_LIST: function () {
+            // console.log(window._config.externalServerCommon);
+            axios.get(window._config.externalServerCommon + '/api/bank')
+                .then(function (response) {
+                    console.log(response);
+                    console.log(1);
+                    store.dispatch({ type: 'GET_COMMON_BANK_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_BUYER_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/buyer')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_BUYER_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_HAMONIZE_LIST: function (data) {
+            axios.get(window._config.externalServerCommon + '/api/groupItem?group_id=9c5514af-407f-4a07-b2c9-8e97f951dd16')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_HAMONIZE_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_CARRIER_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/carrier')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_CARRIER_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_INCOTERMS_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/incoterms')
+                .then(function (response) {
+
+
+                    store.dispatch({ type: 'GET_COMMON_INCOTERMS_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_NOTIFY_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/notify_party')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_NOTIFY_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_PACKAGE_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/package')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_PACKAGE_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_PORT_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/port')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_PORT_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_SHIP_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/ship')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_SHIP_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_SHIPLINE_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/shipline')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_SHIPLINE_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
+        },
+        GET_COMMON_SURVEYOR_LIST: function () {
+            axios.get(window._config.externalServerCommon + '/api/surveyor')
+                .then(function (response) {
+                    store.dispatch({ type: 'GET_COMMON_SURVEYOR_LIST', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log('error');
+                    console.log(error);
+                });
         },
     }
     ]
--- public/script/reduxStore/contract.store.js
+++ public/script/reduxStore/contract.store.js
@@ -1,12 +1,15 @@
 import axios from '../axios'
 import { commonAction } from '../config'
 const initialState = {
-    buyerList: []
+    buyerList: [],
+    contractList: []
 }
 export function contractReducer(state = initialState, action) {
     switch (action.type) {
         case 'GET_BUYER_LIST':
             return Object.assign({}, state, { buyerList: action.payload });
+        case 'GET_CONTRACT_OF_BUYER':
+            return Object.assign({}, state, { contractList: action.payload });
         default:
             return state
     }
@@ -15,7 +18,7 @@ export function contractAction(store) {
     return [commonAction(),
     {
         // GET
-        GET_BUYER_LIST: function (ref, com) {
+        GET_BUYER_LIST: function () {
             axios.get('./contract/list')
                 .then(function (response) {
                     // console.log(response.data);
@@ -24,7 +27,20 @@ export function contractAction(store) {
                 .catch(function (error) {
                     console.log(error);
                 });
-            // console.log(window._config);
+            // console.log(window._config.externalServerCommon);
+            // https://localhost:3001/api/contract/list
+        },
+        // https://localhost:3001/api/contract/buyer?id=44e7a3ae-a7cf-44e3-a9fe-ff8c7a3421cf
+        GET_CONTRACT_OF_BUYER: function (buyerId,) {
+            axios.get('./contract/buyer?id='+buyerId)
+                .then(function (response) {
+                    // console.log(response.data);
+                    store.dispatch({ type: 'GET_CONTRACT_OF_BUYER', payload: response.data })
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            // console.log(window._config.externalServerCommon);
             // https://localhost:3001/api/contract/list
         },
         // END GET
