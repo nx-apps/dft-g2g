ae15d048556213c68453be0c8e764f42edb779dc
Nutthawut-Ice <moomookingza@gmail.com>
2017-06-30 16:21:36 +0700

    update calcFee

--- public/script/i18n/fee.i18n.js
+++ public/script/i18n/fee.i18n.js
@@ -1,5 +1,40 @@
 export const fee ={
     th:{
-        
+        rice_detail_and_payout:"รายละเอียดค่าข้าวและค่าใช้จ่ายส่งมอบ",
+        received_date:"วันที่ได้รับเงิน",
+        cal_no:"งวดที่",
+        exchange_rate:"อัตราแลกเปลี่ยน",
+        exchange_rate_bank:"อัตราแลกเปลี่ยนที่ธนาคารเสนอ",
+        usd:"USD",
+        thb:"THB",
+        baht:"บาท",
+        fee_out:"ค่าธรรมเนียมต่างประเทศ",
+        fee_in:"ค่าธรรมเนียมในประเทศ",
+        received:"จำนวนเงินที่ได้รับ",
+        fee_total:"ค่าธรรมเนียมที่ต้องถัวเฉลี่ย",
+        exporter:"ผู้ส่งออก",
+        value_ton:"มูลค่าต่อตัน",
+        expenses_fee:"หักค่าธรรมเนียม",
+        from:"จาก",
+        invoice_no:"เลขที่ใบแจ้งหนี้",
+        vessel_name:"ชื่อเรือ",
+        amount:"จำนวน",
+        money:"เงิน",
+        ton:"ตัน",
+        ship_lot:"ลำดับเรือที่",
+        expenses_one_present:"หักภาษีรายได้ 1 %",
+        balance:"คงเหลือ",
+        average_money:"การถัวเฉลี่ยเงิน",
+        round_pay:"รอบการจ่ายเงิน",
+        status:"สถานะ",
+        total:"รวม",
+        conclude:"สรุปผล",
+        add:"เพิ่ม",
+        reset_data:"คืนค่าข้อมูลเดิม",
+        back:"ย้อนกลับ",
+        edit:"แก้ไข",
+        confirm:"ยืนยัน",
+        save:"บันทึก",
+        cancel:"ยกเลิก"
     }
-}
+}   
