b7c8034ee0a74ebca98750111bed53e8cdb51deb
Nutthawut-Ice <moomookingza@gmail.com>
2017-07-03 11:33:11 +0700

    update data fee

--- public/script/reduxStore/fee.store.js
+++ public/script/reduxStore/fee.store.js
@@ -17,7 +17,20 @@ export function feeAction(store) {
         FEE_CALC_DATA: function (data) {
             axios.get('./fee/calc?book_id='+data)
             .then((response) => {
-                store.dispatch({type:'FEE_CALC_DATA', payload : response.data});
+                let newData = {
+                    rate_bank_b : 0,
+                    rate_lt_b : 0,
+                    book: response.data
+                }
+                if(typeof newData.fee_date !== 'undefined'){
+                    var date = newData.fee_date.split('T')[0];
+                    newData.fee_date = date;
+                }else{
+                    var dateNow = new Date().toISOString().split('T')[0];
+                    newData.fee_date = dateNow;
+                }
+                // console.log(newData);
+                store.dispatch({type:'FEE_CALC_DATA', payload : newData});
             })
             .catch((err) =>{
                 console.log(err);
