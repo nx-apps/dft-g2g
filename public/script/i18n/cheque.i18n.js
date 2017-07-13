20641de5fa61b4a639e35b5ff0e196ecef11a832
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-07-13 15:05:46 +0700

    update namecontract and fix budge

---
+++ public/script/i18n/cheque.i18n.js
@@ -0,0 +1,13 @@
+export const cheque = {
+    th: {
+        send_cheque: "นำส่งเลขที่เช็ค",
+        send_director: "นำส่งอธิบดี",
+        wait_director: "รออธิบดีอนุมติ",
+        name_of_contract:"ชื่อสัญญา",
+        fee_no:"คิดค่าธรรมเนียมครั้งที่",
+        period_no: "งวดที่",
+        order_no: "ครั้งที่",
+        total_company: "เหลือบริษัท",
+        no_data:"ไม่มีข้อมูล",
+    }
+}   
