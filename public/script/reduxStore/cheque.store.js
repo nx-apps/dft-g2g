8e49f6c0af3ca25d0862eb0440556c31a427061d
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-07-14 17:28:24 +0700

    update cheque

--- public/script/reduxStore/cheque.store.js
+++ public/script/reduxStore/cheque.store.js
@@ -15,11 +15,11 @@ const cutDataInObject = (data, namePop) => {
 }
 const initialState = {
     list: [],
-    dataUrl : [],
+    dataUrl: [],
     cheque_list_fasle: [],
     cheque_list_true: [],
     cheque_detail_list: [],
-    cheque_no:[]
+    cheque_no: []
 }
 export function chequeReducer(state = initialState, action) {
     switch (action.type) {
@@ -41,57 +41,64 @@ export function chequeAction(store) {
     return [commonAction(),
     {
         // SET
-        SET_CHEQUE_DETAIL_LIST : function (data) {
+        SET_CHEQUE_DETAIL_LIST: function (data) {
             store.dispatch({ type: 'SET_CHEQUE_DETAIL_LIST', payload: data })
         },
         // END SET
         // GET
         GET_CHEQUE_LIST: function (contract_idStatus) {
-            axios.get('./cheque/contract?'+contract_idStatus)
+            axios.get('./cheque/contract?' + contract_idStatus)
                 .then(function (response) {
                     // console.log(contract_idStatus)
                     // console.log(response.data);
-                    response.data.map((item)=>{
-                        item.check =  false,
-                        item.status =  false
+                    response.data.map((item) => {
+                        item.check = false,
+                            item.status = false
                     })
                     // console.log(contract_idStatus.search(String('true')));
-                    if(contract_idStatus.search(String('true')) > -1){
+                    if (contract_idStatus.search(String('true')) > -1) {
                         // console.log('รอเซ็น'+ response.data.length);
-                       response.data =  response.data.map((item)=>{
-                            item = cutDataInObject(item, [ 'deliver_date'])
-                           return item
+                        response.data = response.data.map((item) => {
+                            item = cutDataInObject(item, ['deliver_date'])
+                            return item
                         })
                         store.dispatch({ type: 'GET_CHEQUE_LIST_TRUE', payload: response.data })
                         // เซ็ตค่าว่าง
                         // store.dispatch({ type: 'GET_CHEQUE_LIST_FALSE', payload: [{cl_no:1}] })
-                    }else {
+                    } else {
+                        response.data.map((item) => {
+                            if (item.cheque_count === 0 ) {
+                                item.status = true
+                            }
+                        })
                         store.dispatch({ type: 'GET_CHEQUE_LIST_FALSE', payload: response.data })
                         // console.log('ยัง'+ response.data.length);
                         // เซ็ตค่าว่าง
                         // store.dispatch({ type: 'GET_CHEQUE_LIST_TRUE', payload: [{cl_no:1}] })
                     }
-                    
+
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
         },
         GET_CHEQUE_DETAIL_LIST: function (fee) {
-            axios.post('./cheque/fee',fee)
+            axios.post('./cheque/fee', fee)
                 .then(function (response) {
-                    response.data.map((item)=>{
-                        item.check =  false
-                        if (item.invoice_company_date !== undefined){
+                    response.data.map((item) => {
+                        item.check = false
+                        if (item.invoice_company_date !== undefined) {
                             item.invoice_company_date = item.invoice_company_date.split('T')[0]
                         }
                         if (item.cheque_status !== true) {
-                            item.cheque_status =false
+                            item.cheque_status = false
                         }
                         if (item.pay_no === undefined) {
-                            item.pay_no =''
+                            item.pay_no = ''
                         }
+
                     })
+                    console.log(response.data);
                     store.dispatch({ type: 'GET_CHEQUE_DETAIL_LIST', payload: response.data })
                 })
                 .catch(function (error) {
@@ -110,15 +117,15 @@ export function chequeAction(store) {
         // END GET
         // PUT
         PUT_CHEQUE_DETAIL_LIST: function (fee) {
-            return axios.put('./cheque/update',fee)
+            return axios.put('./cheque/update', fee)
         },
         PUT_REJECT_CHEQUE_LIST: function (fee) {
             // console.log(fee);
-            return axios.put('./cheque/approve',fee)
+            return axios.put('./cheque/approve', fee)
         },
         PUT_APPROVE_CHEQUE_LIST: function (fee) {
             // console.log(fee);
-            return axios.put('./cheque/approve',fee)
+            return axios.put('./cheque/approve', fee)
         }
         // PUT
     }
