79a912f85e1e76454f768e883fde5741ae879f33
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-07-18 19:05:52 +0700

    update paid

--- public/script/reduxStore/payment.store.js
+++ public/script/reduxStore/payment.store.js
@@ -1,12 +1,19 @@
 import axios from '../axios'
 import { commonAction } from '../config'
 const initialState = {
-    list: []
+    list: [],
+    listPayment: [],
+    paymentDetail:[],
+    dataSetPayment:{},
 }
 export function paymentReducer(state = initialState, action) {
     switch (action.type) {
-        // case 'EXPORTER_GET_DATA':
-        //     return Object.assign({}, state, { list: action.payload });
+        case 'PAYMENT_GET_LIST_DATA':
+            return Object.assign({}, state, { listPayment: action.payload });
+        case 'PAYMENT_GET_DETAIL_DATA':
+            return Object.assign({}, state, { paymentDetail: action.payload });
+        case 'SET_DATE_PAYMENY':
+            return Object.assign({}, state, { dataSetPayment: action.payload });
         default:
             return state
     }
@@ -14,9 +21,36 @@ export function paymentReducer(state = initialState, action) {
 export function paymentAction(store) {
     return [commonAction(),
     {
-        UPLOAD_GET_LIST: function (ref, com) {
-            // return axios.get('/external/upload/list/' + ref + '/' + com)
+        PAYMENT_GET_LIST_DATA: function (id) {
+            axios.get('./payment/contract?id='+id)
+            .then((response) => {
+                // response.data.map((val) => {
+                //     return val.check = false
+                // })
+                store.dispatch({type: 'PAYMENT_GET_LIST_DATA', payload: response.data})
+            })
         },
+        PAYMENT_GET_DETAIL_DATA: function (contractTaxno) {
+            axios.get('./payment/company?'+contractTaxno)
+            .then((response) => {
+                // response.data.map((val) => {
+                //     return val.check = false
+                // })
+                store.dispatch({type: 'PAYMENT_GET_DETAIL_DATA', payload: response.data})
+            })
+        },
+        SET_DATE_PAYMENY: function (){
+            let set = {
+                pay_value_b:0,
+                bank_id:'KTB',
+                bank:{},
+                pay_status:true,
+                bank_branch:'กระทรวงพาณิชย์',
+                paid_date: new Date().toISOString().split('T')[0]
+            }
+            console.log(set);
+            store.dispatch({type: 'SET_DATE_PAYMENY', payload: set})
+        }
     }
     ]
 }
