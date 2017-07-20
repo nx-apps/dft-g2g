f2c7ddb16542d2c693cc820a6af8d9e5dde9407c
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-07-20 11:53:03 +0700

    update report

---
+++ public/script/i18n/payment.i18n.js
@@ -0,0 +1,8 @@
+export const payment = {
+    th: {
+        company_taxno:"เลขประจำตัวผู้เสียภาษี",
+        company_name_th:"ชื่อบริษัท",
+        cheque_count:"จำนวนเช็ค",
+        no_data:"ไม่มีข้อมูล",
+    }
+}   
