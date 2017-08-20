95bc64c41fe3917b39c718182d336c9f32eeb08f
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-08-20 23:33:41 +0700

    update payment

--- public/script/i18n/payment.i18n.js
+++ public/script/i18n/payment.i18n.js
@@ -1,8 +1,18 @@
 export const payment = {
     th: {
+        text_of_contract :"ชื่อสัญญา",
         company_taxno:"เลขประจำตัวผู้เสียภาษี",
         company_name_th:"ชื่อบริษัท",
         cheque_count:"จำนวนเช็ค",
+        ch_cheque:"เลือกเช็ค",
+        no_cheque:"เลขที่เช็ค",
+        value_bal_b:"จำนวนเงินที่ได้",
+        value_tax_b:"หักภาษี 1 %",
+        value_final_b:"จำนวนเงินที่จ่าย",
+        invoice_company_no:"เลขที่ใบแจ้งนี้",
+        pay_running:"เลขที่ใบหักภาษี ณ ที่จ่าย",
         no_data:"ไม่มีข้อมูล",
+        total:"รวม",
+        save:"บันทึก"
     }
 }   
